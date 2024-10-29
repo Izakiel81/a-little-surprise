ab00a7b7b2d864b68d520cbb0889b12abc303fbf
Maksym <mgabin348@gmail.com>
2024-10-29 02:18:13 +0100

    lines added to note

--- src/components/main-page/heart/note/Note.tsx
+++ src/components/main-page/heart/note/Note.tsx
@@ -3,7 +3,13 @@ import styles from "./Note.module.scss";
 const Note = () => {
   return (
     <div className={styles.note__container}>
-      <span className={styles.note} />
+      <span className={styles.note}>
+        <div className={styles.line}/>
+        <div className={styles.line}/>
+        <div className={styles.line}/>
+        <div className={styles.line}/>
+        <div className={styles.line}/>
+      </span>
     </div>
   );
 };
